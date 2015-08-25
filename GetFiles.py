d422643c9164e285f59d25a15468b4514fef9f1f
Eduardo San Miguel Garcia <abx88as@gmail.com>
2015-08-25 11:47:16 -0500

    Updated README

--- GetFiles.py
+++ GetFiles.py
@@ -4,14 +4,9 @@ class GetFiles():
 
     def getFile(self, dlFile, fromUrl, verbose=0):
         head = requests.head(fromUrl)
-        #print head.headers
-        #req = urllib2.Request(fromUrl)
-        #header_request = req.get_method = lambda : 'HEAD'
-        #header_response = urllib2.urlopen(header_request)
         loop = 1
         existSize = 0
         overwrite_confirmed = False
-        ##myUrlclass = GetFiles(  )
         custom_header = {}
         webSize = int(head.headers['content-length'])
         if os.path.exists(dlFile):
@@ -31,42 +26,27 @@ class GetFiles():
                     print "Please write a correct answer."
                     continue
             outputFile = open(dlFile,"ab")
-
-            # If the file exists, then download only the remainder
-            #req.add_header("Range","bytes={}-{}".format([existSize, header_response.info()['Content-Length']]))
-            ##myUrlclass.addheader("Range","bytes=%s-" % (existSize))
             custom_header = {"Range":"bytes=%s-%s" %((existSize, head.headers['content-length']))}
         else:
             if (overwrite_confirmed):
                 os.remove(outputFile)
-            outputFile = open(dlFile,"wb")
-        ##webPage = myUrlclass.open(fromUrl)
-        #print custom_header
+                outputFile = open(dlFile,"wb")
         response = requests.get(fromUrl, headers=custom_header, stream=True)
-        #print response.headers
         if verbose:
             for k, v in response.headers.items():
                 print k,"=", v
-            
-        #If we already have the whole file, there is no need to download it again
         numBytes = 0
-        ##webSize = int(webPage.headers['Content-Length'])
-        #webSize = int(head.headers['content-length'])
+    
+        
         if webSize == existSize:
             if verbose: print "File (%s) was already downloaded from URL (%s)"%(dlFile, fromUrl)
         else:
             if verbose: print "Downloading %d more bytes" % (webSize-existSize)
-            #while 1:
+            
             for chunk in response.iter_content(8192):
                 outputFile.write(chunk)
                 numBytes = numBytes + len(chunk)
-               
-                #data = response.content(8192)
-                #if not data:
-                #    break
-                #outputFile.write(data)
-                #numBytes = numBytes + len(data)
-                
+
         response.close(  )
         outputFile.close(  )
                     
--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # GetFiles
+
+## Description
+
+GetFiles is a small python program that saves the contents of a URL to
+a file or resumes the download if previously incomplete. It uses the
+[requests](http://www.python-requests.org/en/latest/) HTTP library.
+
+GetFiles works by making a request to the URL and saving its contents
+to a file. The secret sauce lies in checking up the file size in bytes
+in a HEAD request, to later compare it to the current file size on
+disk (if existent). If file already exists, a Range header is sent
+along the request and the reponse object's content returned is streamed
+according to the Range header values.
+
+##To-do
+
+  - HTTP Error handling.
+  - Catch keyboard interrumption.
+  - Use hash for file content check.
+  - Download bar or percentage complete update
+
+## Sources
+
+- [Requests library](http://www.python-requests.org/en/latest/)
+- https://bytedebugger.wordpress.com/2014/05/19/tutorial-how-to-download-and-resume-files-using-python-and-urllib/
+- https://www.safaribooksonline.com/library/view/python-cookbook/0596001673/ch11s06.html
